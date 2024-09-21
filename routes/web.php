470a8aac8d0c70426a72f43cc7e3a7a84f82b29a
ahamedsabbir <isratahanedsabbir@gmail.com>
2024-09-21 17:36:42 +0600

    add role permission route

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,11 @@
 <?php
 
+use App\Http\Controllers\Backend\PermissionController;
+use App\Http\Controllers\Backend\RoleController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
+use Spatie\Permission\Contracts\Permission;
+use Spatie\Permission\Contracts\Role;
 
 Route::get('/', function () {
     return view('welcome');
@@ -16,5 +20,16 @@ Route::middleware('auth')->group(function () {
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
+
+
+Route::resource('roles', RoleController::class)->middleware(['auth', 'verified']);
+Route::resource('permissions', PermissionController::class)->middleware(['auth', 'verified']);
+
+
+
+
+
+
+
 
 require __DIR__.'/auth.php';
